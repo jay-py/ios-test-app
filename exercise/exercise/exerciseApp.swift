15de4830b13c61b1ab0a4ba4a06b749ef18c31eb
jay_py <jeanpaull@live.it>
2023-12-05 15:43:51 +0100

    initial commit

---
+++ exercise/exercise/exerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  exerciseApp.swift
+//  exercise
+//
+//  Created by Jean paul on 2023-12-05.
+//
+
+import SwiftUI
+
+@main
+struct exerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
